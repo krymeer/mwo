c1a732e77bab194de624748e3d02ba0d6b6e0bd5
krymeer <krymeer@gmail.com>
2017-11-18 09:38:06 +0100

    Dodanie komponentu popupu

--- app.new/src/app.vue
+++ app.new/src/app.vue
@@ -1,5 +1,6 @@
 <template>
  <div id="main_container" class="grid">
+    <popup :ifShowPopup="showPopup" :message="popupMessage"></popup>
     <todoHeader></todoHeader>
     <login></login>
     <todoFooter></todoFooter>
@@ -10,9 +11,17 @@
 import { Login } from './user'
 import todoHeader from './todoHeader.vue'
 import todoFooter from './todoFooter.vue'
+import popup from './popup.vue'
 export default {
+  data: function() {
+    return {
+      showPopup: false,
+      popupMessage: '',
+      listOfMessages: ['Żadne pole nie może pozostać puste.', 'To pole nie może pozostać puste.', 'Nieprawidłowa nazwa użytkownika lub hasło.', 'Wylogowanie przebiegło pomyślnie.']
+    };
+  },
   components: {
-    Login, todoHeader, todoFooter
+    Login, todoHeader, todoFooter, popup
   }
 }
 </script>
---
+++ app.new/src/popup.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="popup" v-show="ifShowPopup">{{ message }}</div>
+</template>
+
+<script>
+  export default {
+    props: {
+      ifShowPopup: {
+        type: Boolean,
+        default: false
+      },
+      message: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style></style>

6d637269ce7235fb5abc80f9cbd7f477db525d1d
Krzysztof Tatarynowicz <ktatarynowicz@hotmail.com>
2017-11-19 22:28:37 +0100

    Vue dla braku notatek

---
+++ app.new/src/noNotes.vue
@@ -0,0 +1,23 @@
+<template>
+	<div v-show="no_notes" id="no_notes">
+		Nie masz jeszcze żadnych notatek.
+		<button @click="add_note">dodaj nową</button>
+	</div>
+</template>
+
+<script>
+	import auth from './user/auth'
+	export default {
+		computed: {
+			no_notes: function() {
+				return true;
+			}
+		},
+		methods: {
+			add_note: function() {
+				// TODO: send addNote request
+				// auth.addNote().then();
+			}
+		}
+	}
+</script>

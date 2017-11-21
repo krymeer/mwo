c3729c329f7342335f65031e5e04fa00294f1b84
Marcin Regulski <maarcin.regulski@gmail.com>
2017-11-21 22:00:33 +0100

    pobieranie danych zalogowanego usera, wylogowania
    Nowe metody w auth:
    
    * getUser - zwraca Promise rozwiązujący się na null jeśli nie ma zalogowanego użytkownika, lub na {idToken, userID} w przeciwnym wypadku
    
    * signOut - wylogowuje obecnego usera

--- app.new/src/user/auth.js
+++ app.new/src/user/auth.js
@@ -14,10 +14,12 @@ const promiseCallback = (resolve, reject) => (err, result) => {
     resolve(result)
 }
 
-function promisify(f) {
-    return  f(args, callback)
-}
-
+/**
+ * Register a new user
+ * @param {string} username
+ * @param {string} password
+ * @param {string} email
+ */
 const signUp = (username, password, email) => {
     let attributes = [
         new CognitoUserAttribute({ Name: 'email', Value: email })
@@ -26,17 +28,57 @@ const signUp = (username, password, email) => {
         .then(result => result.user)
 }
 
+/**
+ * Confirm a user's registration
+ * @param {string} username
+ * @param {string} code confirmation code
+ */
 const confirm = (username, code) => {
     let user = new CognitoUser({ Username: username, Pool: userPool })
     return new Promise((resolve, reject) => user.confirmRegistration(code, true, promiseCallback(resolve, reject)))
 }
 
+/**
+ * Try to log a user in. Must be already confirmed.
+ * @param {*} username
+ * @param {*} password
+ */
 const signIn = (username, password) => {
     let authDetails = new AuthenticationDetails({ Username: username, Password: password })
     let user = new CognitoUser({ Username: username, Pool: userPool })
     return new Promise((resolve, reject) => user.authenticateUser(authDetails, { onSuccess: resolve, onFailure: reject }))
 }
 
+/**
+ * Get the currently logged in user.
+ * Returns a promise
+ */
+const getUser = () => {
+    let user = userPool.getCurrentUser()
+    if (user == null) {
+        return Promise.resolve(null)
+    }
+    return Promise.all([
+        new Promise((resolve, reject) => user.getSession(promiseCallback(resolve, reject))),
+        new Promise((resolve, reject) => user.getUserAttributes(promiseCallback(resolve, reject)))
+    ]
+    ).then(results => {
+        let idToken = results[0].idToken
+        let userID = results[1].filter(x => x.Name === 'sub')[0]
+        return { idToken, userID }
+    })
+
+    // return new Promise((resolve, reject) => user.getSession(promiseCallback(resolve, reject)))
+    //     .then(session => ({ user, session }))
+    //     .then(result => new Promise((resolve, reject) => ({ user: result.user, session: result.session, attributes: result.user.getUserAttributes(promiseCallback(resolve, reject)) })))
+    //     .then(console.log)
+}
+
+const signOut = () => {
+    let user = userPool.getCurrentUser()
+    user.signOut()
+}
+
 export default {
-    signUp, confirm, signIn
+    signUp, confirm, signIn, getUser, signOut
 }

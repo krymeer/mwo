d87290f7d940e4a60e5f3d037cd6cf2e0826c053
R3R3 <fiat126pelx@wp.pl>
2017-11-23 23:46:17 +0100

    delete handler

--- server/handlers/delete_todo.js
+++ server/handlers/delete_todo.js
@@ -1,9 +1,22 @@
 'use strict';
-
+const AWS = require('aws-sdk');
 const common = require('./common.todo')
+// DynamoDB API
+const dynamo = common.dynamo();
 
 exports.handler = (event, context, callback) => {
-    console.log('Received DELETE event:', JSON.stringify(event, null, 2));
-
-    callback(common.makeResponse(new Error("NOT IMPLEMENTED")))
+    console.log('Received GET event:', JSON.stringify(event, null, 2));
+    
+    common.getTableName('Todos')
+        .then(actualName => {
+            return dynamo.delete({
+                TableName: actualName,
+                Key:{
+                    "ID": event.pathParameters.id
+                }
+            }).promise()
+        })
+        .then(data => callback(null, common.makeResponse(null, data)))
+        .catch(error => callback(null, common.makeResponse(error, null)))
+    
 }

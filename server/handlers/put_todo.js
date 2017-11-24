e3c273708affdea996ceb2c1cb6ac6fa82435977
R3R3 <fiat126pelx@wp.pl>
2017-11-24 01:10:15 +0100

    handler dla PUT

--- server/handlers/put_todo.js
+++ server/handlers/put_todo.js
@@ -1,9 +1,28 @@
 'use strict';
-
+const AWS = require('aws-sdk');
 const common = require('./common.todo')
+// DynamoDB API
+const dynamo = common.dynamo();
 
 exports.handler = (event, context, callback) => {
     console.log('Received PUT event:', JSON.stringify(event, null, 2));
-
-    callback(common.makeResponse(new Error("NOT IMPLEMENTED")))
+    
+    common.getTableName('Todos')
+        .then(actualName => {
+            return dynamo.update({
+                TableName: actualName,
+                Key:{
+                    "ID": event.pathParameters.id
+                },
+                UpdateExpression: "set Content = :c",
+                ExpressionAttributeValues:{
+                    ":c": event.body.Content
+                },
+                ConditionExpression: "attribute_exists(ID)",
+                ReturnValues:"UPDATED_NEW"
+            }).promise()
+        })
+        .then(data => callback(null, common.makeResponse(null, data)))
+        .catch(error => callback(null, common.makeResponse(error, null)))
+    
 }

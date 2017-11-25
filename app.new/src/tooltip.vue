2a4e22d322cacdeb4415b573f207fc47c709720f
krymeer <krymeer@gmail.com>
2017-11-25 20:44:32 +0100

    Ujednolicenia cd.: zmiana snake case na camel case

--- app.new/src/tooltip.vue
+++ app.new/src/tooltip.vue
@@ -1,7 +1,7 @@
 <template>
 	<span class="tooltip">
 		<span class="hidden" style="display: none">{{ hiddenContents }}</span>
-		<span class="visible" v-on:mouseout="mouse_out" v-on:mouseover="mouse_over">{{ contents }}</span>
+		<span class="visible" v-on:mouseout="mouseOut" v-on:mouseover="mouseOver">{{ contents }}</span>
 	</span>
 </template>
 
@@ -19,39 +19,39 @@
 			}
 		},
 		methods: {
-			mouse_over: function( e ) {
-        var parent            = e.target.parentElement,
-        		parent_rect				= parent.getBoundingClientRect(),
-            parent_t          = parent.offsetTop,
-            parent_l          = parent.offsetLeft,
-            tooltip_contents  = e.target.previousElementSibling;
+			mouseOver: function( e ) {
+        var parent          = e.target.parentElement,
+        		parentRect			= parent.getBoundingClientRect(),
+            parentTop       = parent.offsetTop,
+            parentLeft      = parent.offsetLeft,
+            tooltipContents = e.target.previousElementSibling;
 
-        tooltip_contents.style.display = 'block';
+        tooltipContents.style.display = 'block';
 
-        var tooltip_h     	= tooltip_contents.offsetHeight,
-            tooltip_w     	= tooltip_contents.offsetWidth,
-            tooltip_top   	= (parent_t - tooltip_h) + 'px',
-            tooltip_left  	= parent_l + 'px',
-            tooltip_right		= '',
-            tooltip_bottom	= '';
+        var tooltipHeight = tooltipContents.offsetHeight,
+            tooltipWidth  = tooltipContents.offsetWidth,
+            tooltipTop   	= (parentTop - tooltipHeight) + 'px',
+            tooltipLeft  	= parentLeft + 'px',
+            tooltipRight	= '',
+            tooltipBottom	= '';
 
-        if (parent_rect.left + tooltip_w >= window.innerWidth) {
-          tooltip_left = '';
-          tooltip_right = '0px';
+        if (parentRect.left + tooltipWidth >= window.innerWidth) {
+          tooltipLeft = '';
+          tooltipRight = '0px';
         }
 
-        if (parent_rect.top - tooltip_h <= 0) {
-          tooltip_bottom = '';
-          tooltip_top = (parent_t + tooltip_h) + 'px';
+        if (parentRect.top - tooltipHeight <= 0) {
+          tooltipBottom = '';
+          tooltipTop = (parentTop + tooltipHeight) + 'px';
         }
 
-        tooltip_contents.style.top = tooltip_top;
-        tooltip_contents.style.left = tooltip_left;
-        tooltip_contents.style.right = tooltip_right;
-        tooltip_contents.style.bottom = tooltip_bottom;
+        tooltipContents.style.top = tooltipTop;
+        tooltipContents.style.left = tooltipLeft;
+        tooltipContents.style.right = tooltipRight;
+        tooltipContents.style.bottom = tooltipBottom;
 			},
 
-			mouse_out: function( e ) {
+			mouseOut: function( e ) {
 				e.target.parentElement.childNodes[0].style.display = 'none';
 			}
 		}
